d252810ef10445c0f13b4f6ce110c9600e57c2cd
andrey <andrey.druzhkin@gmail.com>
2022-04-21 11:51:13 -0600

    Add docker container for backend server

--- service/cmd/main.go
+++ service/cmd/main.go
@@ -15,7 +15,7 @@ import (
 )
 
 var (
-	jwtSecret = "jwt_secret_dev"
+	jwtSecret = os.Getenv("JWT_SECRET")
 	port      = flag.Int("port", 5000, "http port to listen on")
 	timeout   = flag.Duration("timeout", 5*time.Second, "timeout for graceful shutdown")
 )
@@ -27,9 +27,16 @@ func main() {
 	s.JWT = &jwt.Token{Secret: jwtSecret}
 	s.DB = &db.Postgres{}
 
-	err := s.DB.Open()
-	if err != nil {
-		log.Fatalf("cannot open db connection: %s\n", err)
+	for i := 0; i < 3; i++ {
+		// Pause before connection to wait for db docker container build
+		time.Sleep(5 * time.Second)
+		err := s.DB.Open()
+		if err != nil {
+			log.Printf("cannot open db connection: %s\n", err)
+			continue
+		}
+
+		break
 	}
 	defer s.DB.Close()
 
@@ -38,7 +45,7 @@ func main() {
 
 	go func() {
 		log.Println("server is up and running")
-		if err = s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("unexpected shuttdown: %s\n", err)
 		}
 	}()
--- service/db/config.go
+++ service/db/config.go
@@ -2,15 +2,16 @@ package db
 
 import (
 	"fmt"
+	"os"
 )
 
 // Environment variables to connect to db.
 var (
-	dbHost = "localhost"
-	dbPort = "15432"
-	dbName = "atm"
-	dbUser = "postgres"
-	dbPass = "mypass"
+	dbHost = os.Getenv("DB_HOST")
+	dbPort = os.Getenv("DB_PORT")
+	dbName = os.Getenv("DB_NAME")
+	dbUser = os.Getenv("DB_USER")
+	dbPass = os.Getenv("DB_PASS")
 )
 
 var pgDSN = fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
