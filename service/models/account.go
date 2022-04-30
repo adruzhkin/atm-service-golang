3be2d8922a4a84924b25c86eb74eb83e347991eb
andrey <andrey.druzhkin@gmail.com>
2022-04-30 15:58:28 -0600

    Add account generation func

--- service/models/account.go
+++ service/models/account.go
@@ -1,10 +1,32 @@
 package models
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type Account struct {
 	ID           int           `json:"id"`
 	Number       string        `json:"number"`
 	Balance      string        `json:"balance,omitempty"`
 	Transactions []Transaction `json:"transactions,omitempty"`
+}
+
+func GenerateAccountNo(previousAccNo string) string {
+	var pointer int
+	for i, v := range previousAccNo {
+		if v == '0' {
+			continue
+		}
+		pointer = i
+		break
+	}
+
+	previousAccNoTrimmed := previousAccNo[pointer:]
+	previousAccNoInt, _ := strconv.Atoi(previousAccNoTrimmed)
+	previousAccNoInt++
+
+	return fmt.Sprintf("%012d", previousAccNoInt)
 }
 
 type AccountRequestBody struct {
